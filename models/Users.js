351cc0733cac30c468a89417ba3e69919d735a19
omiinaya <omiinaya@gmail.com>
2020-05-29 16:45:04 -0400

    added basic user model.

---
+++ models/Users.js
@@ -0,0 +1,26 @@
+const Sequelize = require("sequelize")
+const db = require(".../database/db")
+
+module.exports = db.squelize.define(
+    'user',
+    {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: Sequelize.STRING
+        },
+        password: {
+            type: Sequelize.STRING
+        },
+        created: {
+            type: Sequelize.DATE,
+            defaultValue: Sequelize.NOW
+        }
+    },
+    {
+        timestamps: false
+    }
+)
