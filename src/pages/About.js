eb1d3dd7e0d84b77de07030afcc0506bcfdb65f1
omiinaya <omiinaya@gmail.com>
2020-05-29 21:10:27 -0400

    test

---
+++ src/pages/About.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
+
+function About() {
+  return (
+    <div>
+      <Hero backgroundImage="https://images.unsplash.com/photo-1549814892-f6a520afc97d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60">
+        <h1>Vent-on</h1>
+        <h2>Write your troubles down</h2>
+      </Hero>
+      <Container style={{ marginTop: 30 }}>
+        <Row>
+          <Col size="md-12">
+            <h1>Welcome To EYE-VENT!</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="md-12">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet diam tortor, id
+              
+            </p>
+            <p>
+              Phasellus at rutrum nisl. Praesent sed massa ut ipsum bibendum porttitor. Sed
+              malesuada molestie velit ac viverra. Quisque a ullamcorper purus. Curabitur luctus mi
+              ac mi hendrerit semper. Nulla tincidunt accumsan lobortis. Mauris convallis sapien non
+              nibh porta accumsan. Nunc volutpat tempus porttitor. Nunc congue dictum egestas.
+              
+            </p>
+            Aliquam blandit mi eu urna scelerisque, vitae volutpat ligula ultricies. Maecenas vel
+              porta augue. Fusce mauris ex, dignissim et lacinia ut, tempus eget nibh.
+            <p>
+              
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default About;
