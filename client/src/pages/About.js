f7c845dc11ab237ff7ff575aaa68379a6609902a
Arobl034 <anthonyrobles@Anthonys-iMac.attlocal.net>
2020-05-29 22:24:02 -0400

    cards added

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,42 +1,79 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Container from "../components/Container";
-import Row from "../components/Row";
-import Col from "../components/Col";
+
 
 function About() {
   return (
     <div>
       <Hero backgroundImage="https://images.unsplash.com/photo-1549814892-f6a520afc97d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60">
-        <h1>Vent-on</h1>
+        <h1>venTTown</h1>
         <h2>Write your troubles down</h2>
       </Hero>
       <Container style={{ marginTop: 30 }}>
-        <Row>
-          <Col size="md-12">
-            <h1>Welcome To EYE-VENT!</h1>
-          </Col>
-        </Row>
-        <Row>
-          <Col size="md-12">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet diam tortor, id
-              
-            </p>
-            <p>
-              Phasellus at rutrum nisl. Praesent sed massa ut ipsum bibendum porttitor. Sed
-              malesuada molestie velit ac viverra. Quisque a ullamcorper purus. Curabitur luctus mi
-              ac mi hendrerit semper. Nulla tincidunt accumsan lobortis. Mauris convallis sapien non
-              nibh porta accumsan. Nunc volutpat tempus porttitor. Nunc congue dictum egestas.
-              
-            </p>
-            Aliquam blandit mi eu urna scelerisque, vitae volutpat ligula ultricies. Maecenas vel
-              porta augue. Fusce mauris ex, dignissim et lacinia ut, tempus eget nibh.
-            <p>
-              
-            </p>
-          </Col>
-        </Row>
+      <div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Vented</h5>
+        <p class="card-text"> Old lady (55) looking for older redditers to chat. (Not blocking anyone, though)
+            Covid isolation is doing me in. So tired of being locked in home with my adult kids and my pets. A bit buzzed, who wants to chat? </p>
+        <a href="/" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Vented</h5>
+        <p class="card-text">Lately, it’s been more than difficult to watch the news and it is becoming even harder to bear witness to our country’s slow and agonizing destruction. It is as if we are passengers on a plane that has been hijacked by a group...</p>
+        <a href="/" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</div>
+      <div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Vented</h5>
+        <p class="card-text"> Old lady (55) looking for older redditers to chat. (Not blocking anyone, though)
+            Covid isolation is doing me in. So tired of being locked in home with my adult kids and my pets. A bit buzzed, who wants to chat? </p>
+        <a href="/" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Vented</h5>
+        <p class="card-text">Lately, it’s been more than difficult to watch the news and it is becoming even harder to bear witness to our country’s slow and agonizing destruction. It is as if we are passengers on a plane that has been hijacked by a group...</p>
+        <a href="/" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</div>
+      <div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Vented</h5>
+        <p class="card-text"> Old lady (55) looking for older redditers to chat. (Not blocking anyone, though)
+            Covid isolation is doing me in. So tired of being locked in home with my adult kids and my pets. A bit buzzed, who wants to chat? </p>
+        <a href="/" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Vented</h5>
+        <p class="card-text">Lately, it’s been more than difficult to watch the news and it is becoming even harder to bear witness to our country’s slow and agonizing destruction. It is as if we are passengers on a plane that has been hijacked by a group...</p>
+        <a href="/" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</div>
       </Container>
     </div>
   );
