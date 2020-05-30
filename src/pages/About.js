4a1ed85732fd820c978762d2bb409fe53a82165a
Arobl034 <robles305@gmail.com>
2020-05-29 20:31:20 -0400

    bg image

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,7 +7,7 @@ import Col from "../components/Col";
 function About() {
   return (
     <div>
-      <Hero backgroundImage="https://i.imgur.com/qkdpN.jpg">
+      <Hero backgroundImage="https://images.unsplash.com/photo-1549814892-f6a520afc97d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60">
         <h1>Vent-on</h1>
         <h2>Write your troubles down</h2>
       </Hero>
