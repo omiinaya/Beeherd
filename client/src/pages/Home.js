01b4d2e1f760b4717b3110994adf2897b61fa486
Life is Now <keylasan28@yahoo.com>
2020-06-01 14:56:59 -0400

    trying again path, added Get Informed button to Resources page in Home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,6 +8,10 @@ function Home() {
       <Hero backgroundImage="https://images.unsplash.com/photo-1549814892-f6a520afc97d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60">
         <h1>venTTown</h1>
         <h2>Leave your troubles here</h2>
+        
+                <a href="/resources" className="btn btn-primary">
+                  Get Informed
+                </a>
       </Hero>
       <Container style={{ marginTop: 30 }}>
         <div className="row">
