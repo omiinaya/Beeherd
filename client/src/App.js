55d0f01b0a2eeb4209720092f11ed91045b8aa78
Omar Minaya <omiinaya@gmail.com>
2020-06-04 01:39:13 -0400

    added avatar model.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 //dependencies
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import "./App.css";
 
 //components
 import Navbar from "./components/Navbar/";
---
+++ models/Avatar.js
@@ -0,0 +1,51 @@
+const Sequelize = require("sequelize")
+const db = require("../database/db")
+
+var Avatars = db.sequelize.define(
+    'avatar',
+    {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        owner_id: {
+            type: Sequelize.STRING
+        },
+        skin: {
+            type: Sequelize.STRING
+        },
+        hair: {
+            type: Sequelize.STRING
+        },
+        face: {
+            type: Sequelize.STRING
+        },
+        eyes: {
+            type: Sequelize.STRING
+        },
+        top: {
+            type: Sequelize.STRING
+        },
+        bottom: {
+            type: Sequelize.STRING
+        },
+        background: {
+            type: Sequelize.STRING
+        },
+        emotion: {
+            type: Sequelize.STRING
+        },
+        created: {
+            type: Sequelize.DATE,
+            defaultValue: Sequelize.NOW
+        }
+    },
+    {
+        timestamps: false
+    }
+)
+
+Avatars.sync()
+
+module.exports = Avatars
