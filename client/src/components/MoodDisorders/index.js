0137da80c08e0a86fa8920d6fe0c7c50c9d6578b
Life is Now <keylasan28@yahoo.com>
2020-06-08 18:53:50 -0400

    mood

--- client/src/components/MoodDisorders/index.js
+++ client/src/components/MoodDisorders/index.js
@@ -6,30 +6,81 @@ function MoodDisorders() {
   return (
     
         
+    
     <div className="about">
-    <div class="container">
-      <div class="row align-items-center my-5">
-        <div class="col-lg-7">
+    <div class="container-fluid">
+      <div class="">
+      <h1 class="font-weight-light">Feeling Down?</h1>
+        
+        <div class="col-lg-10">
+          
+          <h2 class="font-weight-light">It's OK To Talk About Depression </h2>
+          <a className="text-support" href="https://www.crisistextline.org">Text to Talk - Completely Confidential</a>
+          <div class="col-lg-7">
           <img
             class="img-fluid rounded mb-4 mb-lg-0"
-            src="http://placehold.it/900x400"
+            src="https://media.clickorlando.com/photo/2019/03/13/suicidehotlineweb_1552496394301_21530477_ver1.0_1280_720.jpg"
             alt=""
           />
         </div>
-        <div class="col-lg-5">
-          <h1 class="font-weight-light">Mood Disorders</h1>
-          <h2 class="font-weight-light">and Depression</h2>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of
-            type and scrambled it to make a type specimen book.
-          </p>
+      <div>
+        <h3>
+        Feeling Down Lately?
+        </h3>
+        <p>
+        You are enough. You matter. Your life is worth fighting for. Suicide is a leading cause of death. It’s also preventable. In U.S. alone, we have the ability to save nearly 45,000 lives every year.
+<br></br>
+Suicide is a leading cause of death. It’s also preventable. In U.S. alone, we have the ability to save nearly 45,000 lives every year. Globally, that number is closer to 800,000.
+<br></br>
+But, hold up. Before we get to saving the world, let’s check-in. If you’re feeling like ending your life, you can always ask for help managing your emotions. Asking for help is brave. You are brave. You are valuable You are important. And, the world needs you.
+        </p>
+        <h5>
+        How to Get Help If You’re Struggling with Mood Disorders
+ </h5>
+
+ <p>
+ Here are a few ways to get through the next second, minute, hour, and even a whole day.
+
+Text us. We’re excellent listeners if we do say so ourselves. And, we’re here for you—always. Text HOME to 741741 to connect with a real human. No judgment—just straight-up listening.
+<br></br>
+Tell a friend. No matter how alone you feel, know that there are so many people in this world who not only want you in this world. And, they want to help you (Hi—have you met us? We’re included in this group! See above). Confiding in someone can help you build a support system for times when you are feeling in a really dark place. Nervous about reaching out? Start with a text message like this: “Hey there. I’m feeling really alone right now. Would you mind keeping me company?”
+
+<br></br>Distract yourself. Find even one thing that brings you joy. And, hold onto it. Maybe it’s listening to your favorite song (listening to music is one of the most common coping tools for our texters), or maybe it’s looking into the eyes of your adorable pup. Whatever it may be, find one thing to turn to when you feel in the darkest of places.
+
+<br></br>Talk to a pro. If you are thinking about ending your life, chances are you are dealing with really painful emotions. Nobody deserves to feel that way and we are so sorry you do. Reach out to a therapist who can help you work through and process your emotions in a healthy way.
+<br></br>Go somewhere safe. If you feel like you are a danger to yourself, it’s always okay (brave, even!) to call 911 or go to an ER.
+ </p>
+ <div className="container-hotline1">
+ Text HOME to 741741 to connect with a Crisis Counselor
+ <br></br>
+Free 24/7 support at your fingertips
+<br></br>
+US and Canada: text 741741
+<br></br>
+UK: text 85258 | Ireland: text 086 1800 280
+ </div>
+<h6>Requesting Support?</h6>
+<h7>We Can Locate The Nearest Support Center For You</h7>
+ <div class="input-group mb-3">
+  <input type="text" class="form-control" placeholder="Enter your Zipcode" aria-label="Requesting Support?" aria-describedby="button-addon2"/>
+  <div class="input-group-append">
+    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Help</button>
+  </div>
+</div>
+ <div class="col-lg-5">
+          <img
+            class="img-fluid rounded mb-6 mb-lg-0"
+            src="https://www.bettycjung.net/BG2018/SuicideWarningSigns.jpg"
+            alt=""
+          />
         </div>
+      </div>
+         </div>
       </div>
     </div>
   </div>
 );
-}
+
+  }
 
 export default MoodDisorders;
