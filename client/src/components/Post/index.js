1a1e17a98f8a6598fa18b7667a840441e981a4be
Arobl034 <anthonyrobles@Anthonys-iMac.attlocal.net>
2020-06-11 17:50:27 -0400

    Editor N/C

--- client/src/components/Post/index.js
+++ client/src/components/Post/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { upload } from '../UserFunctions'
 import jwt_decode from 'jwt-decode'
 import "./style.css";
+import Editor from '../../components/Editor';
 
 class Post extends Component {
     constructor() {
@@ -45,11 +46,10 @@ class Post extends Component {
     render() {
         return (
             <div class="container">
-                <div className="col-md-6 mt-5 mx-auto">
+                
+                <Editor />
                     <form noValidate onSubmit={this.onSubmit}>
-                        <h1 id="headerTitle">WIP</h1>
                         <div className="form-group">
-                            <label htmlFor="content">Post</label>
                             <input type="content"
                                 className="form-control"
                                 name="content"
@@ -63,7 +63,6 @@ class Post extends Component {
                             Submit
                             </button>
                     </form>
-                </div>
             </div>
         )
     }
