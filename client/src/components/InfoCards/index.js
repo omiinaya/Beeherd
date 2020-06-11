3bd84d7bf2d56394e300a2808096c459e7d7d995
Life is Now <keylasan28@yahoo.com>
2020-06-11 13:47:09 -0400

    made InfoCards component for my pages- animation works-- [iGetTo]=> recreate multiple for use. It's displayed in /addiction page for testing, if youre curious.

---
+++ client/src/components/InfoCards/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function InfoCards() {
+  return (
+    <div className="infoCardsMain">
+      <div className="infoCardsCard">
+        <div className="infoCardsFront">Why should I choose my life?</div>
+        <div className="infoCardsBack">Because you matter, little bee.. even if you don't believe me.</div>
+      </div>
+    </div>
+  );
+}
+export default InfoCards;
--- client/src/pages/Addiction/index.js
+++ client/src/pages/Addiction/index.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react'
 import "./style.css";
 import Words from '../../components/Words';
 import "../../components/Words/style.css";
+import InfoCards from '../../components/InfoCards';
+
 function Addiction(){
  {
     return (
-     <Words />
+    <InfoCards />
     );
   }
 }
