086af029a6aa88f5a4806bf9e7416e36acd3fc4c
Life is Now <keylasan28@yahoo.com>
2020-06-12 12:24:03 -0400

    final html skeleton done for MoodsPage. [iGet2]=> add css and recreate other pages with same skeleton for designing consistency. All pages seem to be done tonight. Im excited.

---
+++ client/src/components/Words/indexWord.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Words() {
+  return (
+    <div className="wordsDiv">
+      <h1 className="wordsText1">It's OK to feel<span></span></h1>
+  
+    </div>
+  );
+}
+export default Words;
