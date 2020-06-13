66184aa69e8704982b24115e771bd94f169dccbf
Life is Now <keylasan28@yahoo.com>
2020-06-13 19:08:22 -0400

    almost there

--- client/src/components/Words/index.js
+++ client/src/components/Words/index.js
@@ -67,78 +67,126 @@ function Words() {
           </div>
         </section>
 
-        <div className="moodTextDiv2">
-          <p className="moodTextp2">Some examples of mood disorders include:</p>
+      
+          <div className="moodTextDiv2">
+            <p className="moodTextp2">
+              Some examples of mood disorders include:
+            </p>
+            <ul className="Moodul">
+              <li className="Moodli">
+                Major depressive disorder — prolonged and persistent periods of
+                extreme sadness
+              </li>
+              <li className="Moodli">
+                Bipolar disorder — also called manic depression or bipolar
+                affective disorder, depression that includes alternating times
+                of depression and mania
+              </li>
 
-          <p className="moodText3">
-            Major depressive disorder — prolonged and persistent periods of
-            extreme sadness <br></br>Bipolar disorder — also called manic
-            depression or bipolar affective disorder, depression that includes
-            alternating times of depression and mania
-            <br></br>
-            Seasonal affective disorder (SAD) — a form of depression most often
-            associated with fewer hours of daylight in the far northern and
-            southern latitudes from late fall to early spring <br></br>
-            Cyclothymic disorder — a disorder that causes emotional ups and
-            downs that are less extreme than bipolar disorder
-            <br></br>
-            Premenstrual dysphoric disorder — mood changes and irritability that
-            occur during the premenstrual phase of a woman's cycle and go away
-            with the onset of menses Persistent depressive disorder (dysthymia)
-            — a long-term (chronic) form of depression
-            <br></br>
-            Disruptive mood dysregulation disorder — a disorder of chronic,
-            severe and persistent irritability in children that often includes
-            frequent temper outbursts that are inconsistent with the child's
-            developmental age <br></br>Depression related to medical illness — a
-            persistent depressed mood and a significant loss of pleasure in most
-            or all activities that's directly related to the physical effects of
-            another medical condition <br></br>Depression induced by substance
-            use or medication ― depression symptoms that develop during or soon
-            after substance use or withdrawal or after exposure to a medication
-          </p>
-        </div>
+              <li className="Moodli">
+                Cyclothymic disorder — a disorder that causes emotional ups and
+                downs that are less extreme than bipolar disorder
+              </li>
 
-        <div className="moodTextDiv3">
-          <h5 className="moodText4">When to see a doctor</h5>
+              <li className="Moodli">
+                Seasonal affective disorder (SAD) — a form of depression most
+                often associated with fewer hours of daylight in the far
+                northern and southern latitudes from late fall to early spring
+              </li>
+              <li className="Moodli">
+              Premenstrual dysphoric disorder — mood changes and irritability
+                that occur during the premenstrual phase of a woman's cycle and
+                go away with the onset of menses Persistent depressive disorder
+                (dysthymia) — a long-term (chronic) form of depression
+              </li>
+              <li className="Moodli">
+              Disruptive mood dysregation disorder — a disorder of chronic,
+                severe and persistent irritability in children that often
+                includes frequent temper outbursts that are inconsistent with
+                the child's developmental age
+              </li>
 
-          <p className="moodText5">
-            Talk to a health care professional if you: Feel like your emotions
-            are interfering with your work, relationships, social activities or
-            other parts of your life Have trouble with drinking or drugs Have
-            suicidal thoughts or behaviors — seek emergency treatment
-            immediately Your mood disorder is unlikely to simply go away on its
-            own, and it may get worse over time. Seek professional help before
-            your mood disorder becomes severe — it may be easier to treat early
-            on.<br></br>If you are thinking about ending your life, or having
-            suicidal thoughts due to your circumstances and state of mind,
-            chances are you are dealing with really painful emotions. Nobody
-            deserves to feel that way and we are so sorry you do. Reach out to a
-            therapist who can help you work through and process your emotions in
-            a healthy way.
-          </p>
-        </div>
+              <li className="Moodli">
+              Depression related to
+                medical illness — a persistent depressed mood and a significant
+                loss of pleasure in most or all activities that's directly
+                related to the physical effects of another medical condition
+              </li>
 
-        <section className="MoodSection3">
-          <div className="MoodSec3container">
-            <div className="moodBox1">
-              <p className="moodText2">and here some more </p>
+              <li className="Moodli">
+              Depression induced by substance use or medication ―
+                depression symptoms that develop during or soon after substance
+                use or withdrawal or after exposure to a medication
+              </li>
+            </ul>
             </div>
-            <div className="moodBox2">
-              <p className="moodText2">and here some more </p>
             </div>
-            <div className="moodBox3">
-              <p className="moodText2">and here some more </p>
+          
+
+
+ 
+
+
+
+            <div className="moodText3div">
+              <p className="moodText3">
+               
+              </p>
+            </div>
+
+            <div className="moodTextDiv3">
+              <h5 className="moodText4">When to see a doctor</h5>
+
+              <p className="moodText5">
+                Talk to a health care professional if you: Feel like your
+                emotions are interfering with your work, relationships, social
+                activities or other parts of your life Have trouble with
+                drinking or drugs Have suicidal thoughts or behaviors — seek
+                emergency treatment immediately Your mood disorder is unlikely
+                to simply go away on its own, and it may get worse over time.
+                Seek professional help before your mood disorder becomes severe
+                — it may be easier to treat early on.<br></br>If you are
+                thinking about ending your life, or having suicidal thoughts due
+                to your circumstances and state of mind, chances are you are
+                dealing with really painful emotions. Nobody deserves to feel
+                that way and we are so sorry you do. Reach out to a therapist
+                who can help you work through and process your emotions in a
+                healthy way.
+              </p>
+            </div>
+
+            <section className="MoodSection3">
+              <div className="MoodSec3container">
+                <div className="moodBox1">
+                  <p className="moodText2">and here some more </p>
+                </div>
+                <div className="moodBox2">
+                  <p className="moodText2">and here some more </p>
+                </div>
+                <div className="moodBox3">
+                  <p className="moodText2">and here some more </p>
+                </div>
+              </div>
+            </section>
+            <footer className="informedFooter">
+              <p className="informedFooterText">
+                Life Is Now, Copyright &copy; beeherd 2020
+              </p>
+            </footer>
+            <div className="infoCardsMain">
+        <div className="infoCardsCard1">
+          <div className="infoCardsFront1">
+           
+            <div className="infoCardsBack1">
+              Because you matter, little bee.. even if you don't believe me.
             </div>
           </div>
-        </section>
-        <footer className="informedFooter">
-          <p className="informedFooterText">
-            Life Is Now, Copyright &copy; beeherd 2020
-          </p>
-        </footer>
-      </div>
-    </div>
+        </div>
+</div>
+          </div>
+        
+  
+    
   );
 }
 
