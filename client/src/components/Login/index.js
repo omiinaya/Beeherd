6e1fade24e41620e3d8159cc5459b732c4c36a34
Arobl034 <anthonyrobles@Anthonys-iMac.attlocal.net>
2020-06-13 16:01:58 -0400

    login animation

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import { login } from '../UserFunctions'
 import "./style.css";
+import Animation from '../../components/Animation'
+
 
 
 class Login extends Component {
@@ -36,13 +38,16 @@ class Login extends Component {
 
     render() {
         return (
-            <div className="container">
+            <div className="container animation">
+                
                 <div id="loginform">
+                    <Animation />
                     <div className="col-md-6 mt-5 mx-auto">
                         <form noValidate onSubmit={this.onSubmit}>
                             <h1 id="headerTitle">Sign In</h1>
 
                             <div className="form-group">
+                                
                                 <label htmlFor="username">Username</label>                          
                                 <input type="username"
                                 className="form-control"
@@ -62,8 +67,10 @@ class Login extends Component {
                                 onChange={this.onChange}
                                 />
                             </div>
+                            
                             <button type="submit"
                             id="button">
+                                
                                 Login
                             </button>
                         </form>
