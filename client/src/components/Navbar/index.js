9e21a43d684cc7c60bfc39991d0dabaf352afd49
omiinaya <omiinaya@gmail.com>
2020-06-14 18:49:17 -0400

    rolled back navbar. new color was not it chief.

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -37,7 +37,7 @@ class Navbar extends Component {
             </ul>
         )
         return (
-            <nav className="navbar navbar-expand-md navbar-dark sticky-top">
+            <nav className="navbar navbar-expand-md navbar-dark bg-dark">
                 <div class="container-fluid">
                 <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
                     <ul className="navbar-nav mr-auto">
