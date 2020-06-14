c350d80e679ce565206fb650081066ac6dac4a17
Life is Now <keylasan28@yahoo.com>
2020-06-14 18:32:46 -0400

    importingPage2

--- client/src/pages/Suicide/index.js
+++ client/src/pages/Suicide/index.js
@@ -1,108 +1,204 @@
 import React from "react";
 import "./style.css";
-
 function Suicide() {
   return (
-    <div className="containChooseLife">
-      <div className="container-fluid">
-        <div classname="uMatterDiv">
-          <h1 className="neon" data-text="[You Matter]">
-            [You_Matter]
-          </h1>
-          <div className="col-lg-10">
-            <h2 className="heading-suicide font-weight-light">
-              It's OK to Talk About Suicide
-            </h2>
-            <a className="text-support" href="https://www.crisistextline.org">
+    <div className="MoodPageContainer">
+      <div className="MoodPage">
+        <header className="moodHeader">
+          <div className="MHcontainer">
+            <h1 className="MHtext1">Let's Talk</h1>
+          </div>
+        </header>
+
+        <section className="MoodSection1">
+          <div className="MoodSec1container">
+            <div className="text-container">
+              <h1 className="moodColors">About Suicide</h1>
+            </div>
+            <h1 className="LifeText1 ">You Matter</h1>
+          </div>
+        </section>
+
+        <section className="MoodSectionPic">
+          <div className="MoodSecPicContainer"></div>
+        </section>
+        <center>
+          <div className="moodTextDiv">
+            <h1 className="mooddown">it's ok to talk about suicide</h1>
+
+            <div className="borderDown">
+              <h1 className="mooddown2">:: now ::</h1>
+            </div>
+          </div>
+          <h3 className="mooddown"></h3>
+        </center>
+        <section className="MoodSectionPic">
+          <div className="MoodSecPicContainer"></div>
+        </section>
+        <p className="moodTextp1">
+          You are enough. You matter. Your life is worth fighting for. Suicide
+          is a leading cause of death. It’s also preventable. In U.S. alone, we
+          have the ability to save nearly 45,000 lives every year. Globally,
+          that number is closer to 800,000. But, hold up. Before we get to
+          saving the world, let’s check-in. If you’re feeling like ending your
+          life, you can always ask for help managing your emotions. Asking for
+          help is brave. You are brave. You are valuable! You are important!
+          And, the world needs you. Please share your gifts with the world,
+          little bee.
+        </p>
+
+        <section className="MoodSection2">
+          <div className="MoodSec2container">
+            <h1 className="requestSupport">Requesting Support?</h1>
+            <p className="locateSupport">
+              We Can Locate The Nearest Support Center For You{" "}
+            </p>
+            <div className="contact-me">
+              <div className="informedInputs">
+                <input
+                  type="text"
+                  className="zipcodeInput "
+                  placeholder="Enter Your Zipcode"
+                ></input>
+                <button className="informedButton">
+                  <span className="informedSpan">Show Me</span>
+                </button>
+              </div>
+            </div>
+            
+        
+          </div>
+          
+        </section>
+     
+
+        <div className="moodTextDiv2">
+          <p className="moodTextp2">Having </p>
+          <p className="moodTextp22">Suicidal Thoughts?</p>
+          <p className="LifeText2">
+          
+           
+          </p>
+          <ul className="Moodul">
+            <li className="Moodli">
+              Text us. We’re excellent listeners if we do say so ourselves. And,
+              we’re here for you—always. Text HOME to 741741 to connect with a
+              real human. No judgment—just straight-up listening. <a className="text-support" href="https://www.crisistextline.org">
               -Text to Talk-
             </a>
-            <div className="col-lg-7">
-              <img
-                className="suicide-hotline img-fluid rounded mb-4 mb-lg-0"
-                src="https://media.clickorlando.com/photo/2019/03/13/suicidehotlineweb_1552496394301_21530477_ver1.0_1280_720.jpg"
-                alt=""
-              />
-            </div>
-            <div classname="chooseLifeText">
-              <h3 classname="CLtext1">Having Suicidal Thoughts?</h3>
-              <p className="CLtext2">
-                You are enough. You matter. Your life is worth fighting for.
-                Suicide is a leading cause of death. It’s also preventable. In
-                U.S. alone, we have the ability to save nearly 45,000 lives
-                every year. Suicide is a leading cause of death. It’s also
-                preventable. In U.S. alone, we have the ability to save nearly
-                45,000 lives every year. Globally, that number is closer to
-                800,000. But, hold up. Before we get to saving the world, let’s
-                check-in. If you’re feeling like ending your life, you can
-                always ask for help managing your emotions. Asking for help is
-                brave. You are brave. You are valuable You are important. And,
-                the world needs you.
-              </p>
-              <h5 className="CLtext3">
-                How to Get Help If You’re Thinking About Ending Your Life
-              </h5>
-              <p className="CLtext4">
-                Here are a few ways to get through the next second, minute,
-                hour, and even a whole day.
-              </p>{" "}
-              <p className="CLtext5">
-                Text us. We’re excellent listeners if we do say so ourselves.
-                And, we’re here for you—always. Text HOME to 741741 to connect
-                with a real human. No judgment—just straight-up listening.
-              </p>
-              <p className="CLtext6">
-                Tell a friend. No matter how alone you feel, know that there are
-                so many people in this world who not only want you in this
-                world. And, they want to help you (Hi—have you met us? We’re
-                included in this group! See above). Confiding in someone can
-                help you build a support system for times when you are feeling
-                in a really dark place. Nervous about reaching out? Start with a
-                text message like this: “Hey there. I’m feeling really alone
-                right now. Would you mind keeping me company?”
-              </p>
-              <br></br>{" "}
-              <p className="CLtext7">
-                Distract yourself. Find even one thing that brings you joy. And,
-                hold onto it. Maybe it’s listening to your favorite song
-                (listening to music is one of the most common coping tools for
-                our texters), or maybe it’s looking into the eyes of your
-                adorable pup. Whatever it may be, find one thing to turn to when
-                you feel in the darkest of places.
-              </p>
-              <br></br>
-              <p className="CLtext8">
-                Talk to a pro. If you are thinking about ending your life,
-                chances are you are dealing with really painful emotions. Nobody
-                deserves to feel that way and we are so sorry you do. Reach out
-                to a therapist who can help you work through and process your
-                emotions in a healthy way.
-              </p>
-              <br></br>
-              <p className="CLtext5">
-                Go somewhere safe. If you feel like you are a danger to
-                yourself, it’s always okay (brave, even!) to call 911 or go to
-                an ER.
-              </p>
-            </div>
-            <div className="container-hotline1">
+            </li>
+            <li className="Moodli">
+              Tell a friend. No matter how alone you feel, know that there are
+              so many people in this world who not only want you in it, but they
+              want to help you.
+            </li>
+
+            <li className="Moodli">
+              (Hi—have you met us? We’re included in this group! See above)
+              Here, at beeherd, we are committed to helping you see you are
+              powerful enough to choose life no matter what shows up in your
+              space!
+            </li>
+
+            <li className="Moodli">
+              Confiding in someone can help you build a support system for times
+              when you are feeling in a really dark place. Nervous about
+              reaching out? Start with a text message like this: “Hey there. I’m
+              feeling really alone right now. Would you mind keeping me
+              company?”
+            </li>
+            <li className="Moodli">
+              Distract yourself. Find even one thing that brings you joy. And,
+              hold onto it. Maybe it’s listening to your favorite song
+              (listening to music is one of the most common coping tools for our
+              texters), or maybe it’s looking into the eyes of your adorable
+              pup. Whatever it may be, find one thing to turn to when you feel
+              in the darkest of places.
+            </li>
+            <li className="Moodli">
+              Something that has helped me tremendously is putting my hands over
+              my heart and simply being with the pain that I feel. Something
+              amazing will happen when you trust that feeling is healing.
+            </li>
+
+            <li className="Moodli">
+              Talk to a pro. If you are thinking about ending your life, chances
+              are you are dealing with really painful emotions. Nobody deserves
+              to feel that way and we are so sorry you do. Reach out to a
+              therapist who can help you work through and process your emotions
+              in a healthy way.
+            </li>
+
+            <li className="Moodli">
+              Go somewhere safe. If you feel like you are a danger to yourself,
+              it’s always okay (brave, even!) to call 911 or go to an ER.
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="lifedivCubes">
+        <p className="LifeText3"></p>
+      </div>
+
+      <div className="moodTextDiv3">
+        <p className="moodText5">YOU</p>
+        <p className="moodText555">are Not</p>
+        <p className="moodText55">alone</p>
+      </div>
+
+      <section className="MoodSection3">
+        <div className="MoodSec3container MoodPage">
+          <div className="moodBox">
+            <p className="moodTextbox">
               Text HOME to 741741 to connect with a Crisis Counselor
               <br></br>
               Free 24/7 support at your fingertips
               <br></br>
               US and Canada: text 741741
               <br></br>
-              UK: text 85258 | Ireland: text 086 1800 280
-            </div>
-            <div className="locateUserDiv">
-              <h6 className="requestSupport">Requesting Support?</h6>
-              <h7 className="locateSupport">
-                We Can Locate The Nearest Support Center For You
-              </h7>
-              
-            </div>
+              UK: text 85258 | Ireland: text 086 1800 280 <br></br>
+              Let us locate support nearby by providing your zipcode above.
+            </p>
           </div>
+          <div className="moodBox">
+            <p className="moodTextbox">
+              Text to talk -- Completely Confidential
+              <span className="crisisHotline">
+                Text HOME to 741741: US & CANADA
+              </span>
+              <span className="moodTextbox">
+                Your life is worth living. I know it must not feel like that
+                right now. I invite you to place your hands over your heart and
+                after a few deep breaths,
+              </span>
+              <span className="crisisHotline">ask yourself: Who am I?</span>
+            </p>
+          </div>
+
+          <div className="moodBox">
+            <p className="moodTextbox">
+              If you are thinking about ending your life due to your
+              circumstances and state of mind, chances are you are dealing with
+              really painful emotions. Nobody deserves to feel that way and we
+              are so sorry you do. We're here to help. You are not alone.
+            </p>
+          </div>
+          <div classname="uMatterDiv">
+          <h1 className="neon" data-text="[You Matter]">
+            [You_Matter]
+          </h1> </div>
+
         </div>
-      </div>
+      </section>
+      <section className="Informedfoot">
+        <footer className="informedFooter">
+      
+          <p className="informedFooterText">
+            Life Is Now, Copyright &copy; beeherd 2020
+          </p>
+        </footer>
+      </section>
     </div>
   );
 }
