61c77ee43b75e336d7f9edbc7e4702b0b5e2fa85
Omar Minaya <omiinaya@gmail.com>
2020-06-15 11:02:07 -0400

    added showing replies. n/c

--- client/src/components/ExpandPost/index.js
+++ client/src/components/ExpandPost/index.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic"
+//import ClassicEditor from "@ckeditor/ckeditor5-build-classic"
 import { sendToDB } from "./scripts"
-import CKEditor from "@ckeditor/ckeditor5-react"
+//import CKEditor from "@ckeditor/ckeditor5-react"
 import axios from "axios"
+import { Row, Col } from "../Grid";
+import { PostList, PostListItem } from "../PostList";
+import EmptyList from "../EmptyList";
+import SeeMoreButton from "../SeeMoreButton";
 import "./style.css"
 
 class ExpandPost extends React.Component {
@@ -11,10 +15,13 @@ class ExpandPost extends React.Component {
         this.handleClick = this.handleClick.bind(this);
         this.state = {
             toggleReply: false,
+            savedReplies: [],
+            initialized: true,
             post: null
         };
     }
     componentDidMount() {
+        this.getReplies();
         var id = this.props.match.params.id;
         axios.get(`/posts/` + id)
             .then(res => {
@@ -47,22 +54,51 @@ class ExpandPost extends React.Component {
 
     getReplies = () => {
         axios
-          .get("/replies/all")
-          .then((res) => {
-            this.setState({ savedReplies: res.data });
-            //console.log(res.data)
-          })
-          .catch((err) => console.log(err));
-      };
+            .get("/replies/all")
+            .then((res) => {
+                this.setState({ savedReplies: res.data });
+                console.log(res.data)
+            })
+            .catch((err) => console.log(err));
+    };
 
     render() {
         const { toggleReply, post, title, author, id } = this.state;
         const defaultView = (
-            <div className="expanded-post-container">
-                <div className="post_author">Author: {author}</div>
-                <div className="post_title">Title: {title}</div>
-                <div dangerouslySetInnerHTML={{ __html: post }} className="post_content" />
-                <button onClick={this.handleClick}>Reply</button>
+            <div>
+                <div className="expanded-post-container">
+                    <div className="post_author">Author: {author}</div>
+                    <div className="post_title">Title: {title}</div>
+                    <div dangerouslySetInnerHTML={{ __html: post }} className="post_content" />
+                    <button onClick={this.handleClick}>Reply</button>
+                </div>
+                <Col size="md-9">
+                    {this.state.savedReplies.length > 0 ?
+                        <PostList>
+                            {this.state.savedReplies.slice(0).reverse().map(replies => {
+                                //console.log(posts)
+                                return (
+                                    <div className="post-card" onClick={() => this.handleOnClick(replies)}>
+                                        <div>
+                                            <PostListItem
+                                                id={replies.author_id}
+                                                author={replies.author_tag}
+                                                content={replies.reply_content}
+                                            />
+                                            <div className="button-container">
+                                                <SeeMoreButton
+                                                    id={replies.id}
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                );
+                            })}
+                        </PostList>
+                        :
+                        <EmptyList />
+                    }
+                </Col>
             </div>
         )
         const clickedView = (
--- client/src/components/TagSection/scripts.js
+++ client/src/components/TagSection/scripts.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+var temp_tag;
+var wordList;
+var userCheck;
+var wordListFixed = [];
+
 export function changeTag(a) {
    var input = document.getElementById("change-tag").value;
     axios.put('users/id/'+a, {
@@ -10,4 +15,44 @@ export function changeTag(a) {
     }).catch(err => {
         console.log(err)
     });
+}
+
+export function generateWordList() {
+    axios({
+        method: 'get',
+        url: 'https://random-word-api.herokuapp.com/word?number=30',
+    })
+        .then((res => {
+            //console.log(res.data)
+            wordList = res.data
+        })
+        )
+}
+
+export function capitalizeFirst() {
+    for (var i = 0; i < wordList.length; i++) {
+        var upperCased = wordList[i].charAt(0).toUpperCase() + wordList[i].slice(1).toLowerCase();
+        wordListFixed.push(upperCased);
+    }
+}
+
+export function generateRandomTag() {
+    capitalizeFirst()
+    var newTag = [];
+
+    for (var i = 0; i < 3; i++) {
+        newTag.push(wordListFixed[Math.floor(Math.random() * wordListFixed.length)])
+    }
+
+    temp_tag = newTag.join("");
+    //console.log(temp_tag);
+}
+
+export function isUser(a) {
+    return axios
+    .get('users/' + a)
+    .then(function (res) {
+        userCheck = res.data
+        return userCheck;
+    })
 }
