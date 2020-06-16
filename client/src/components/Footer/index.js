0ed2826c1e268709794a69bcc2067d342ff7735d
Omar Minaya <omiinaya@gmail.com>
2020-06-16 03:20:24 -0400

    fixed footer having a weird color gap between emoji and top edge

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,15 +4,17 @@ import "./style.css";
 class Footer extends Component {
   render() {
     return (
-      <footer className="footer">
-        <div className="container-fluid padding">
+      <footer className="footer-container">
+        <div className="container-fluid">
           <div className="row">
             <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
-            <span role="img" aria-label="peace">✌️</span>
-            <ul>
-              <li><a href="https://github.com/omiinaya/OKAJ">github</a></li>
-              <li><a href="https://github.com/omiinaya/OKAJ">&nbsp; site made by Team OKAJ</a></li>
-            </ul>
+              <div className="footer">
+                <span role="img" aria-label="peace">✌️</span>
+                <ul>
+                  <li><a href="https://github.com/omiinaya/OKAJ">github</a></li>
+                  <li><a href="https://github.com/omiinaya/OKAJ">&nbsp; site made by Team OKAJ</a></li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
@@ -20,5 +22,5 @@ class Footer extends Component {
     );
   }
 }
- 
+
 export default Footer;
