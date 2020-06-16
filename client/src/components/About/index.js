7c97ffb28a9ca7dda0efca2e5bbd2b2f23ca2c98
omiinaya <omiinaya@gmail.com>
2020-06-16 12:38:32 -0400

    shadows EVERYWHERE.

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 //import { BrowserRouter as Router } from "react-router-dom";
+import Footer from '../Footer'
 import "./styles.css";
 import {
   MDBJumbotron,
@@ -23,23 +24,19 @@ function About() {
               <MDBCardTitle className="card-title h4 pb-2">
                 <strong>beeherd</strong>
               </MDBCardTitle>
-
               <MDBCardImage
                 src="https://mdbootstrap.com/img/Photos/Slides/img%20(134).jpg"
                 className="img-fluid"
               />
               <MDBCardBody>
-
                 <MDBCardText className="middle-page">
                   At beeherd posts are the essence of the site. The greatest good you can do for one another is to share and connect with one another.
                   Feel free to resgister anonynously. Sign-in, vent on, and let your
                   voice beeheard.
                 </MDBCardText>
-
                 <MDBCol className="d-flex justify-content-center mt-4" md="12">
                   <MDBCol md="3" className="d-flex justify-content-around">
-                  <MDBBtn href="/" gradient="purple">Posts</MDBBtn>
-                  
+                    <MDBBtn href="/" gradient="purple">Posts</MDBBtn>
                   </MDBCol>
                 </MDBCol>
               </MDBCardBody>
@@ -47,6 +44,7 @@ function About() {
           </MDBCol>
         </MDBRow>
       </MDBContainer>
+      <Footer />
     </div>
   );
 }
--- client/src/components/Avatar/index.js
+++ client/src/components/Avatar/index.js
@@ -16,54 +16,57 @@ class Avatar extends Component {
                     <div class="container">
                         <div class="row">
                             <div class="col-12">
-
-                                <div className="dropdown">
-                                    <button className="avatar-dropbtn">Body ⯆</button>
-                                    <div className="dropdown-content">
-                                        <h6 id="standard1">White</h6>
-                                        <h6 id="standard2">Black</h6>
+                                <div className="dropdown-div">
+                                    <div className="dropdown">
+                                        <button className="avatar-dropbtn">Body ⯆</button>
+                                        <div className="dropdown-content">
+                                            <h6 id="standard1">White</h6>
+                                            <h6 id="standard2">Black</h6>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="dropdown">
-                                    <button className="avatar-dropbtn">Eyes ⯆</button>
-                                    <div className="dropdown-content">
-                                        <h6 id="eyes1">eyes1</h6>
-                                        <h6 id="eyes2">eyes2</h6>
+                                    <div className="dropdown">
+                                        <button className="avatar-dropbtn">Eyes ⯆</button>
+                                        <div className="dropdown-content">
+                                            <h6 id="eyes1">eyes1</h6>
+                                            <h6 id="eyes2">eyes2</h6>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="dropdown">
-                                    <button className="avatar-dropbtn">Style ⯆</button>
-                                    <div className="dropdown-content">
-                                        <h6 id="hair1">Style1</h6>
-                                        <h6 id="hair2">Style2</h6>
+                                    <div className="dropdown">
+                                        <button className="avatar-dropbtn">Style ⯆</button>
+                                        <div className="dropdown-content">
+                                            <h6 id="hair1">Style1</h6>
+                                            <h6 id="hair2">Style2</h6>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="dropdown">
-                                    <button className="avatar-dropbtn">Style ⯆</button>
-                                    <div className="dropdown-content">
-                                        <h6 id="ropa1">Outfit1</h6>
-                                        <h6 id="ropa2">Outfit2</h6>
+                                    <div className="dropdown">
+                                        <button className="avatar-dropbtn">Outfit ⯆</button>
+                                        <div className="dropdown-content">
+                                            <h6 id="ropa1">Outfit1</h6>
+                                            <h6 id="ropa2">Outfit2</h6>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="dropdown">
-                                    <button className="avatar-dropbtn">Style ⯆</button>
-                                    <div className="dropdown-content">
-                                        <h6 id="bb1">Place1</h6>
-                                        <h6 id="bb2">Place2</h6>
+                                    <div className="dropdown">
+                                        <button className="avatar-dropbtn">Place ⯆</button>
+                                        <div className="dropdown-content">
+                                            <h6 id="bb1">Place1</h6>
+                                            <h6 id="bb2">Place2</h6>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="row col-6 this">
-                            <div class="col-9  avatardisplay es99">
-                                <div class="col-12  avatardisplay es1">
-                                    <div class="col-12  avatardisplay es2">
-                                        <div class="col-12  avatardisplay es3">
-                                            <div class="col-12  avatardisplay es4">
-                                                <div class="col-12  avatardisplay es5">
+                        <div class="avatar-container">
+                            <div class="col-6">
+                                <div class="col-9  avatardisplay es99">
+                                    <div class="col-12  avatardisplay es1">
+                                        <div class="col-12  avatardisplay es2">
+                                            <div class="col-12  avatardisplay es3">
+                                                <div class="col-12  avatardisplay es4">
+                                                    <div class="col-12  avatardisplay es5">
 
+                                                    </div>
+                                                    <div class="col-12  avatardisplay"> </div>
                                                 </div>
-                                                <div class="col-12  avatardisplay"> </div>
                                             </div>
                                         </div>
                                     </div>
