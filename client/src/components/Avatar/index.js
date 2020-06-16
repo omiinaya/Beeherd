03806af9efe6037f97b24bb53d2a7f1463efd140
omiinaya <omiinaya@gmail.com>
2020-06-16 11:21:41 -0400

    porting v4

--- client/src/components/Avatar/index.js
+++ client/src/components/Avatar/index.js
@@ -32,32 +32,16 @@ class Avatar extends Component {
                                         <h6 id="avatarmenu">STYLE</h6>
                                         <h6 id="hair1">Style1</h6>
                                         <h6 id="hair2">Style2</h6>
-                                        <h6 id="hair3">Style3</h6>
-                                        <h6 id="hair4">Style4</h6>
-                                        <h6 id="hair5">Style5</h6>
-                                        <h6 id="hair6">Style6</h6>
-                                        <h6 id="hair7">Style7</h6>
-                                        <h6 id="hair8">Style8</h6>
                                     </div>
                                     <div class="col-2 this cloth">
-                                        <h6 id="ropa1">WEAR</h6>
-                                        <h6 id="ropa2">Outfit1</h6>
-                                        <h6 id="ropa3">Outfit2</h6>
-                                        <h6 id="ropa4">Outfit3</h6>
-                                        <h6 id="ropa5">Outfit4</h6>
+                                        <h6 id="avatarmenu">WEAR</h6>
+                                        <h6 id="ropa1">Outfit1</h6>
+                                        <h6 id="ropa2">Outfit2</h6>
                                     </div>
                                     <div class="col-3 this Backround">
-                                        <h6 id="b1">LOCATION</h6>
-                                        <h6 id="bb2">Place1</h6>
-                                        <h6 id="bb3">Place2</h6>
-                                        <h6 id="bb4">Place3</h6>
-                                        <h6 id="bb5">Place4</h6>
-                                        <h6 id="bb6">Place5</h6>
-                                        <h6 id="bb7">Place6</h6>
-                                        <h6 id="bb8">Place7</h6>
-                                        <h6 id="bb9">Place8</h6>
-                                        <h6 id="bb10">Place9</h6>
-                                        <h6 id="bb11">Place10</h6>
+                                        <h6 id="avatarmenu">LOCATION</h6>
+                                        <h6 id="bb1">Place1</h6>
+                                        <h6 id="bb2">Place2</h6>
                                     </div>
                                 </div>
                             </div>
@@ -69,7 +53,7 @@ class Avatar extends Component {
                                         <div class="col-12  avatardisplay es3">
                                             <div class="col-12  avatardisplay es4">
                                                 <div class="col-12  avatardisplay es5">
-                                                    <h6 id="follow9">Create Your avatar</h6>
+                                                    
                                                 </div>
                                                 <div class="col-12  avatardisplay"> </div>
                                             </div>
--- client/src/components/Avatar/scripts.js
+++ client/src/components/Avatar/scripts.js
@@ -50,10 +50,6 @@ function avatarBackend() {
         $("#eyesimage").remove();
     });
 
-    $("#eyes3").mouseenter(function () {
-        $(".es2").append("<img id='eyesimage' src='https://i.imgur.com/RL5B4NA.png'></img>");
-    });
-
     $("#eyes1").click(function () {
         $(".cleani2").remove();
         $("#eyesimage").replaceWith(
@@ -79,50 +75,10 @@ function avatarBackend() {
     });
 
     $("#hair2").mouseenter(function () {
-        $(".es3").append("<img id='hairimage' src=''></img>");
+        $(".es3").append("<img id='hairimage' src='https://i.imgur.com/kCODyoJ.png'></img>");
     });
 
     $("#hair2").mouseleave(function () {
-        $("#hairimage").remove();
-    });
-
-    $("#hair4").mouseenter(function () {
-        $(".es3").append("<img id='hairimage' src='h1.png'></img>");
-    });
-
-    $("#hair4").mouseleave(function () {
-        $("#hairimage").remove();
-    });
-
-    $("#hair5").mouseenter(function () {
-        $(".es3").append("<img id='hairimage' src='h2.png'></img>");
-    });
-
-    $("#hair5").mouseleave(function () {
-        $("#hairimage").remove();
-    });
-
-    $("#hair6").mouseenter(function () {
-        $(".es3").append("<img id='hairimage' src='h3.png'></img>");
-    });
-
-    $("#hair6").mouseleave(function () {
-        $("#hairimage").remove();
-    });
-
-    $("#hair7").mouseenter(function () {
-        $(".es3").append("<img id='hairimage' src='h4.png'></img>");
-    });
-
-    $("#hair7").mouseleave(function () {
-        $("#hairimage").remove();
-    });
-
-    $("#hair8").mouseenter(function () {
-        $(".es3").append("<img id='hairimage' src='h5.png'></img>");
-    });
-
-    $("#hair8").mouseleave(function () {
         $("#hairimage").remove();
     });
 
@@ -136,50 +92,14 @@ function avatarBackend() {
     $("#hair2").click(function () {
         $(".cleani3").remove();
         $("#hairimage").replaceWith(
-            "<img class='cleani3' id='hairimages' src='https://i.imgur.com/ntF7A9G.png'></img>"
-        );
-    });
-
-
-    $("#hair4").click(function () {
-        $(".cleani3").remove();
-        $("#hairimage").replaceWith(
-            "<img class='cleani3' id='hairimages2' src='h1.png'></img>"
-        );
-    });
-
-    $("#hair5").click(function () {
-        $(".cleani3").remove();
-        $("#hairimage").replaceWith(
-            "<img class='cleani3' id='hairimages2' src='h2.png'></img>"
-        );
-    });
-
-    $("#hair6").click(function () {
-        $(".cleani3").remove();
-        $("#hairimage").replaceWith(
-            "<img class='cleani3' id='hairimages2' src='h3.png'></img>"
-        );
-    });
-
-    $("#hair7").click(function () {
-        $(".cleani3").remove();
-        $("#hairimage").replaceWith(
-            "<img class='cleani3' id='hairimages2' src='h4.png'></img>"
-        );
-    });
-
-    $("#hair8").click(function () {
-        $(".cleani3").remove();
-        $("#hairimage").replaceWith(
-            "<img class='cleani3' id='hairimages2' src='h5.png'></img>"
+            "<img class='cleani3' id='hairimages' src='https://i.imgur.com/kCODyoJ.png'></img>"
         );
     });
 
     //ropa/
 
     $("#ropa1").mouseenter(function () {
-        $(".es4").append("<img id='ropaimage' src='dress.png'></img>");
+        $(".es4").append("<img id='ropaimage' src='https://i.imgur.com/LDzjMI6.png'></img>");
     });
 
     $("#ropa1").mouseleave(function () {
@@ -187,127 +107,46 @@ function avatarBackend() {
     });
 
     $("#ropa2").mouseenter(function () {
-        $(".es4").append("<img id='ropaimage' src='outf2.png'></img>");
+        $(".es4").append("<img id='ropaimage' src='https://i.imgur.com/HrHKTCL.png'></img>");
     });
 
     $("#ropa2").mouseleave(function () {
         $("#ropaimage").remove();
     });
 
-    $("#ropa3").mouseenter(function () {
-        $(".es4").append("<img id='ropaimage' src='outf5.png'></img>");
-    });
-
-    $("#ropa3").mouseleave(function () {
-        $("#ropaimage").remove();
-    });
-
-    $("#ropa4").mouseenter(function () {
-        $(".es4").append("<img id='ropaimage' src='outfi4.png'></img>");
-    });
-
-    $("#ropa4").mouseleave(function () {
-        $("#ropaimage").remove();
-    });
-
-    $("#ropa5").mouseenter(function () {
-        $(".es4").append("<img id='ropaimage' src='outfit3.png'></img>");
-    });
-
-    $("#ropa5").mouseleave(function () {
-        $("#ropaimage").remove();
-    });
-
     $("#ropa1").click(function () {
         $(".cleani4").remove();
         $("#ropaimage").replaceWith(
-            "<img class='cleani4' id='ropaimages' src='dress.png'></img>"
+            "<img class='cleani4' id='ropaimages' src='https://i.imgur.com/LDzjMI6.png'></img>"
         );
     });
 
     $("#ropa2").click(function () {
         $(".cleani4").remove();
         $("#ropaimage").replaceWith(
-            "<img class='cleani4' id='ropaimages' src='outf2.png'></img>"
-        );
-    });
-
-    $("#ropa3").click(function () {
-        $(".cleani4").remove();
-        $("#ropaimage").replaceWith(
-            "<img class='cleani4' id='ropaimages' src='outf5.png'></img>"
-        );
-    });
-
-    $("#ropa4").click(function () {
-        $(".cleani4").remove();
-        $("#ropaimage").replaceWith(
-            "<img class='cleani4' id='ropaimages' src='outfi4.png'></img>"
-        );
-    });
-
-    $("#ropa5").click(function () {
-        $(".cleani4").remove();
-        $("#ropaimage").replaceWith(
-            "<img class='cleani4' id='ropaimages' src='outfit3.png'></img>"
+            "<img class='cleani4' id='ropaimages' src='https://i.imgur.com/HrHKTCL.png'></img>"
         );
     });
 
     //background//
 
-    $("#bb2").click(function () {
+    $("#bb1").click(function () {
         $(".es99").css(
             "background-image",
             "url('https://cutewallpaper.org/21/retro-game-backgrounds/Game-background-GIFs-Get-the-best-GIF-on-GIPHY.gif')"
         );
     });
 
-    $("#bb3").click(function () {
-        $(".es99").css("background-image", "url('back9.gif')");
+    $("#bb2").click(function () {
+        $(".es99").css("background-image", "url('https://media.giphy.com/media/ggWxxzrbja9VWsZ45o/giphy.gif')");
         $(".es99").css("background-position", "289px 227px");
         $(".es99").css("background-size", "287px 201px");
     });
 
-    $("#bb4").click(function () {
+    $("#bb3").click(function () {
         $(".es99").css("background-image", "url('dp.gif')");
         $(".es99").css("background-position", "431px 171px");
         $(".es99").css("background-size", "538px 182px");
-    });
-
-    $("#bb5").click(function () {
-        $(".es99").css("background-image", "url('forrest.gif')");
-        $(".es99").css("background-position", "497px 171px");
-        $(".es99").css("background-size", "571px 185x");
-    });
-
-    $("#bb6").click(function () {
-        $(".es99").css("background-image", "url('volcano.gif')");
-        $(".es99").css("background-position", "492px 171px");
-        $(".es99").css("background-size", "254px 143x");
-    });
-
-    $("#bb7").click(function () {
-        $(".es99").css("background-image", "url('castle.gif')");
-        $(".es99").css("background-position", "396px 177px");
-        $(".es99").css("background-size", "547px 227x");
-    });
-
-    $("#bb8").click(function () {
-        $(".es99").css("background-image", "url('bathroom.gif')");
-    });
-
-    $("#bb9").click(function () {
-        $(".es99").css("background-image", "url('washer.gif')");
-    });
-
-    $("#bb10").click(function () {
-        $(".es99").css("background-image", "url('city.gif')");
-        $(".es99").css("background-position", "406px 177px");
-        $(".es99").css("background-size", "598px 293x");
-    });
-
-    $("#bb11").click(function () {
-        $(".es99").css("background-image", "url('waterfall.gif')");
     });
 }
 
