3ce871a5e7ad43764598bc3a9a4db83c9dd6bd57
Omar Minaya <omiinaya@gmail.com>
2020-06-17 12:36:20 -0400

    accidentally changed heroku password and broke it.

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize")
 const db = {}
 
-const sequelize = new Sequelize("qww6dau6olmfkfpz", "ph1g0gudqztccot4", "oy4cudzgfz0o1upw", {
+const sequelize = new Sequelize("qww6dau6olmfkfpz", "ph1g0gudqztccot4", "foyffrvagwr5p1sm", {
     host: 'tk3mehkfmmrhjg0b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
     operatorAliases: false,
