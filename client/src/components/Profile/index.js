066c15f452c160a9655480e3c7df5fe954347a0b
Omar Minaya <omiinaya@gmail.com>
2020-06-17 09:13:53 -0400

    journal frontend mostly done.

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 //import jwt_decode from 'jwt-decode'
+import { Link } from 'react-router-dom'
 import axios from 'axios'
 import Footer from '../Footer';
 import Journals from '../Journals';
@@ -8,9 +9,9 @@ import "./style.css";
 class Profile extends Component {
     constructor(props) {
         super(props)
+        this.handleClick = this.handleClick.bind(this);
         this.state = {
             toggleEditor: false,
-            savedJournals: '',
             onlyOne: '1'
         }
     }
@@ -34,26 +35,66 @@ class Profile extends Component {
         document.getElementById('journals-content').innerText = "";
         for (var i = 0; i < a.length; i++) {
             const div = document.createElement('div');
+            const last = a.length - 1;
             div.setAttribute("class", "journal-card")
             div.innerHTML = ` 
-                <div id="journal_author">Created `+a[0].created+` By: `+a[0].author_tag+`</div>
-                <div id="journal_content">`+a[0].journal_content+`</div>
+                <div id="journal_author">Created `+ a[last].created + ` By: ` + a[last].author_tag + `</div>
+                <div id="journal_content">`+ a[last].journal_content + `</div>
             `;
             document.getElementById('journals-content').appendChild(div);
         }
     }
 
+    handleClick() {
+        if (this.state.toggleEditor === false) {
+            this.setState({
+                toggleEditor: true
+            })
+            console.log(this.state.toggleEditor)
+        } else {
+            this.setState({
+                toggleEditor: false
+            })
+            console.log(this.state.toggleEditor)
+        }
+    }
+
     render() {
-        return (
+        const { toggleEditor } = this.state;
+        const clickedView = (
             <div>
                 <div className="profile-container">
                     <h1 className="profile-title text-center">Profile</h1>
+                    <center>
+                        <div className="mx-auto order-0">
+                            <button className="mx-auto profile-buttons" onClick={() => this.handleClick()}>See your Posts</button>
+                        </div>
+                    </center>
                 </div>
                 <div id="journals-content">There are no journals to display.</div>
                 <div id="journals-container">
                     <Journals />
                 </div>
                 <Footer />
+            </div>
+        )
+        const defaultView = (
+            <div>
+                <div className="profile-container">
+                    <h1 className="profile-title text-center">Profile</h1>
+                    <center>
+                        <div className="mx-auto order-0">
+                            <button className="mx-auto profile-buttons" onClick={() => this.handleClick()}>Write a Journal</button>
+                        </div>
+                    </center>
+                </div>
+                <div id="journals-content">There are no journals to display.</div>
+                <Footer />
+            </div>
+        )
+        return (
+            <div>
+                {toggleEditor ? clickedView : defaultView}
             </div>
         )
     }
