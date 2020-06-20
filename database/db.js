9d940a73b900a93f42c6e68c3a8acd5f333c567d
omiinaya <omiinaya@gmail.com>
2020-06-20 07:09:31 -0400

    dynamically adjusting tag without relogging or reloading page.

--- database/db.js
+++ database/db.js
@@ -1,13 +1,13 @@
 const Sequelize = require("sequelize")
 const db = {}
 
-const sequelize = new Sequelize("qww6dau6olmfkfpz", "ph1g0gudqztccot4", "foyffrvagwr5p1sm", {
-    host: 'tk3mehkfmmrhjg0b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+const sequelize = new Sequelize("okaj_db", "root", "root", {
+    host: 'localhost',
     dialect: 'mysql',
     operatorAliases: false,
 
     pool: {
-        max: 15,   
+        max: 5,
         min: 0,
         acquire: 30000,
         idle: 10000
--- database/heroku.js
+++ database/heroku.js
@@ -1,13 +1,13 @@
 const Sequelize = require("sequelize")
 const db = {}
 
-const sequelize = new Sequelize("okaj_db", "root", "root", {
-    host: 'localhost',
+const sequelize = new Sequelize("qww6dau6olmfkfpz", "ph1g0gudqztccot4", "foyffrvagwr5p1sm", {
+    host: 'tk3mehkfmmrhjg0b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
     operatorAliases: false,
 
     pool: {
-        max: 5,
+        max: 15,   
         min: 0,
         acquire: 30000,
         idle: 10000
